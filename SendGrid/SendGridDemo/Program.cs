19841737ef78bfef1bfde1d669d49aa4c63a81bf
justrun <qiqian8321850@126.com>
2014-09-24 14:42:21 +0800

    The send grid web api demo

---
+++ SendGrid/SendGridDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Mail;
+using SendGrid;
+
+
+namespace SendGridDemo
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Create the email object first, then add the properties.
+            var myMessage = new SendGridMessage();
+            myMessage.AddTo("test@test.com");
+            myMessage.From = new MailAddress("john@example.com", "John Smith");
+            myMessage.Subject = "Testing the SendGrid Library";
+            myMessage.Text = "Hello World!";
+
+            // Create credentials, specifying your user name and password.
+            var credentials = new NetworkCredential("username", "password");
+
+            // Create an Web transport for sending email.
+            var transportWeb = new Web(credentials);
+
+            // Send the email.
+            transportWeb.Deliver(myMessage);
+        }
+    }
+}
